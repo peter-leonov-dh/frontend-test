23c6c3931c85f4097e8ec15b6e0ac172e213ad07
Peter Leonov <gojpeg@gmail.com>
2015-12-31 21:25:28 +0100

    form

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,5 @@ var $ = require('./lib/query')
 var fetch = require('./lib/fetch')
 
 var recipesWidget = require('./recipes-widget/widget')($('#app'), fetch)
+
+var loginFormWidget = require('./login-form-widget/widget')($('#login'))
